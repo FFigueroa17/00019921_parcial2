1f38028e7de4c2fe7e12d424139d868e95822f9a
FFigueroa17 <00019921@uca.edu.sv>
2023-05-23 10:39:15 -0600

    :memo: FIX

--- ACTORS TRACKER/app/src/main/java/com/figueroa/actortracker/ui/actor/ActorInfoFragment.kt
+++ ACTORS TRACKER/app/src/main/java/com/figueroa/actortracker/ui/actor/ActorInfoFragment.kt
@@ -27,7 +27,20 @@ class ActorInfoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_actor_info, container, false)
+        binding = FragmentActorInfoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    // ON VIEW CREATED
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setViewModel()
+    }
+
+    // SET VIEW MODEL
+    private fun setViewModel() {
+        binding.viewmodel = actorsViewModel
     }
 }
